92043d091d362dbdce3103d8dccea87b9c3233b5
lam <quoclam4a@gmail.com>
2023-08-16 16:40:39 +0700

    create card music

--- layouts/index/main.vue
+++ layouts/index/main.vue
@@ -1,12 +1,31 @@
 <template>
-  <div class="w-auto">
+  <div class="w-full">
     <li class="flex">
       <div class="flex">
         <div>
-          <img>
+          <img class="w-48" src="https://cdn-i.vtcnews.vn/upload/2023/07/19/a-loi-23281702.jpg">
         </div>
 
-        <div class="card w-96 bg-base-100 shadow-xl">
+        <div class="card bg-base-100 shadow-xl">
+          <div class="card-body">
+            <h2 class="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+          </div>
+        </div>
+      </div>
+
+      <div>
+
+      </div>
+    </li>
+    <br>
+    <li class="flex">
+      <div class="flex">
+        <div>
+          <img class="w-48" src="https://cdn-i.vtcnews.vn/upload/2023/07/19/a-loi-23281702.jpg">
+        </div>
+
+        <div class="card bg-base-100 shadow-xl">
           <div class="card-body">
             <h2 class="card-title">Shoes!</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
@@ -20,3 +39,6 @@
     </li>
   </div>
 </template>
+
+<script setup lang="ts">
+</script>
